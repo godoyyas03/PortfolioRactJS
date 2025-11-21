e3375722a5e454f286ba23850176aa5e95ced882
godoyyas03 <146505274+godoyyas03@users.noreply.github.com>
2025-11-21 10:14:02 -0300

    Delete 4_Projeto/portifolio-front-end directory

--- 4_Projeto/portifolio-front-end/src/js/validar.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-export const Validar = yup.object().shape({
-    nome: yup.string().required('*Preencher o campo obrigatório NOME!*'),
-    email: yup.string().required('*Preencher o campo obrigatório E-MAIL!*'),
-    ass: yup.string().required('*Preencher o campo obrigatório ASSUNTO!*'),
-    msg: yup.string().required('*Preencher o campo obrigatório MENSAGEM!*'),
-})
